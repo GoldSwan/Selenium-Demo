51f363150ce544f92ae7d940f38a06eb285151cf
GoldSwan <saltmenu@naver.com>
2021-11-16 17:35:28 +0900

    [ADD]Excel 서비스 기능 추가

---
+++ WorkAutomation/WorkAutomation/Service/ExcelService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Excel = Microsoft.Office.Interop.Excel;
+
+//https://docs.microsoft.com/ko-kr/dotnet/csharp/programming-guide/interop/walkthrough-office-programming
+namespace WorkAutomation.Service
+{
+    class ExcelService : IAutomationService
+    {
+        MainForm mainForm { get; set; }
+        public ExcelService(MainForm mainForm)
+        {
+            this.mainForm = mainForm;
+        }
+        public void AutoService()
+        {
+
+        }
+        void DisplayInExcel(string strExcelInputData)
+        {
+            var excelApplication = new Excel.Application();
+            var workBook = excelApplication.Workbooks.Open(mainForm.txtExcelDirectory.Text);
+        }        
+    }
+}
